ad31c14e06abea950622a99fb0cb727650a09c24
Timothy Logan <timtimmahh@gmail.com>
2021-01-05 05:40:06 -0500

    Improving settings web server with reactivity and better configuration manipulation.

--- flaskr/home/updates.py
+++ flaskr/home/updates.py
@@ -4,49 +4,57 @@ from flaskr.home import blueprint
 
 
 def get_device(device):
+    for device_config in config.devices:
+        if device_config.name == device:
+            return device_config
+    return None
 
 
+def parse_post_data(device, form):
+    def update_name(name):
+        if name != device.data.name or name != device.data.capabilities['name']:
+            device.data.name = name
+            device.data.capabilities['name'] = name
+            device.data.last_properties['name'] = name
+            device.data.set_name(name)
+            return True
+        return False
 
-def update_name(device, name):
-    if name != dev_config.data.name or name != dev_config.data.capabilities['name']:
-        dev_config.data.name = name
-        dev_config.data.capabilities['name'] = name
-        dev_config.data.last_properties['name'] = name
-        dev_config.data.set_name(name)
+    def update_effect(effect):
+        if effect != device.data.effect:
+            device.data.effect = effect
+            return True
+        return False
 
+    def update_duration(duration):
+        if duration != device.data.duration:
+            device.data.duration = duration
+            return True
+        return False
 
+    def update_color_flow(color_flow):
+        if color_flow != device.data.color_flow_mode:
+            device.data.color_flow_mode = color_flow
+            return True
+        return False
 
-def parse_post_data(**data):
+    data = dict(
+        name=update_name,
+        effect=update_effect,
+        duration=update_duration,
+        color_flow=update_color_flow
+    )
 
+    should_update = False
+    for arg, func in data:
+        should_update = arg in form and func(form[arg])
+
+    if should_update:
+        config.set_device(device)
 
 
 @blueprint.route('/device/<string:device>/', methods=['GET', 'POST'])
 @blueprint.route('/device/<string:device>', methods=['GET', 'POST'])
-def device_update(device):
-
-
-
-if request.method == 'POST':
-    print(request.form)
-    name = request.form['name']
-    effect = request.form['effect']
-    duration = int(request.form['duration'])
-    color_flow = request.form['color_flow']
-    should_update = False
-        should_update = True
-        print('Updating name')
-    if effect != dev_config.data.effect:
-        dev_config.data.effect = effect
-        should_update = True
-        print('Updating effect')
-    if duration != dev_config.data.duration:
-        dev_config.data.duration = duration
-        should_update = True
-        print('Updating duration')
-    if color_flow != dev_config.data.color_flow_mode:
-        dev_config.data.color_flow_mode = color_flow
-        should_update = True
-        print('Updating color flow')
-
-    if should_update:
-        config.set_device(dev_config)
+def device_update(device, **post_data):
+    if request.method == 'POST':
+        parse_post_data(get_device(device), **post_data)
